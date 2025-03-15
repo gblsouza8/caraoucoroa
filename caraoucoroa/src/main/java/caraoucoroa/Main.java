0d3c5cca54a1b2683d6c531edfefac1a11dddc35
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-15 14:38:56 -0300

    Segundo Commit

--- caraoucoroa/src/main/java/caraoucoroa/Main.java
+++ caraoucoroa/src/main/java/caraoucoroa/Main.java
@@ -1,7 +1,29 @@
 package caraoucoroa;
-
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite a quantidade de vezes que deseja jogar: ");
+        int vezes = input.nextInt();
+
+        int qtd;
+        int cara = 0;
+        int coroa = 0;
+
+        for (qtd = 0; qtd < vezes; qtd++){
+
+            Random nAleatorio = new Random();
+            int numeroAleatorio = nAleatorio.nextInt(1, 3);
+            if (numeroAleatorio == 1)
+            {
+                cara += 1;
+            } else {
+                coroa += 1;
+            }
+        }
+        System.out.println("Quantidade de caras: " + cara + " Quantidade de coroas: " + coroa);
     }
 }
